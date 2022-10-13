58aca1d5486cf81f0f479b81022dda3c6f4fa8be
epineda0304 <epineda0304@gmail.com>
2022-10-13 10:20:15 -0400

    Update R03_NUM03_J.java
    Making code compliant

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,5 @@
 
-public static int getInteger(DataInputStream is) throws IOException 
+public static long getInteger(DataInputStream is) throws IOException 
 {
-    return is.readInt(); 
+    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }

718420874a8d1e4b4ee4cedf7ff5b5451e860656
epineda0304 <epineda0304@gmail.com>
2022-10-12 20:24:29 -0400

    Add files via upload

---
+++ cis4615a.java
@@ -0,0 +1,10 @@
+
+
+public class Cis4615a 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World - from the class of CIS4615 - Fall 2022 - UCF");
+    }
+
+}

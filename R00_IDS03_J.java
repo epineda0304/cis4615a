4741719947a07e45d7be18eca525bff7e2fb1960
epineda0304 <epineda0304@gmail.com>
2022-10-12 20:49:15 -0400

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,10 @@
+
+if (loginSuccessful) 
+{
+    logger.severe("User login succeeded for: " + username);
+} 
+
+else 
+{
+    logger.severe("User login failed for: " + username);
+}
---
+++ R03_NUM03_J.java
@@ -0,0 +1,5 @@
+
+public static int getInteger(DataInputStream is) throws IOException 
+{
+    return is.readInt(); 
+}

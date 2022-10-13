a944414d0fa93827bf4ffa63b98ce4e12e202fff
epineda0304 <epineda0304@gmail.com>
2022-10-13 10:54:37 -0400

    Update R09_LCK04_J.java
    Compliant Solution

--- R09_LCK04_J.java
+++ R09_LCK04_J.java
@@ -1,20 +1,19 @@
-
 private final Map<Integer, String> mapView =
     Collections.synchronizedMap(new HashMap<Integer, String>());
 private final Set<Integer> setView = mapView.keySet();
  
 public Map<Integer, String> getMap() 
 {
-    return mapView;
+  return mapView;
 }
  
 public void doSomething() 
 {
-    synchronized (setView) 
-    {  // Incorrectly synchronizes on setView
-        for (Integer k : setView) 
-        {
-            // ...
-        }
+  synchronized (mapView) 
+  {  // Synchronize on map, rather than set
+    for (Integer k : setView) 
+    {
+      // ...
     }
+  }
 }
